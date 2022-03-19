ec5caf8b344fe52a0fef960d10a523e97a6397ac
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-03-19 13:15:31 -0400

    adding form and dropdown form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav'
 import { NavDropdown } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   return (
@@ -27,6 +28,34 @@ function App() {
           <Nav.Link href='/contact'>Contact Us</Nav.Link>
         </Nav.Item>
       </Nav>
+      <Form>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type='email' placeholder='Enter Email' />
+          <Form.Text className='text-muted'>
+            We do not share user emails with any other parties.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control type='password' placeholder='Password' />
+        </Form.Group>
+        <Form.Group controlId='formBasicCheckbox'>
+          <Form.Check type='checkbox' label='Check me out' />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit Form
+        </Button>
+      </Form>
+      <div>
+        <Form.Group>
+          <Form.Label>Form Dropdown Menu</Form.Label>
+          <Form.Control as='select'>
+            <option>Option 1</option>
+            <option>Option 2</option>
+          </Form.Control>
+        </Form.Group>
+      </div>
     </div>
 
   );
