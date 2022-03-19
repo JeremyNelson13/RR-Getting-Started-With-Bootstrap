04af1074cad9b1a2825719a967aade3b9a1c777a
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-03-19 13:04:12 -0400

    adding styling navbar, button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
-
-
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav'
+import { NavDropdown } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
-     
+      <Button variant='primary'>New Button</Button>
+      <Nav fill variant='tabs' activeKey='/home'>
+        <Nav.Item>
+          <Nav.Link href='/home'>Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href='/shop'>Shop</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <NavDropdown title='About Us' id='nav-dropdown'>
+            <NavDropdown.Item>Locations</NavDropdown.Item>
+          </NavDropdown>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href='/account'>Account</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href='/contact'>Contact Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
+
   );
 }
 
